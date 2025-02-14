8e8ce60864d8164a0bf3ccdde0b1e960850517f6
김희망 <gmlakd990514@naver.com>
2025-02-14 15:19:02 +0900

    feat: 백준 10870번 풀이

---
+++ BaekJoon/bronze/B27433.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B27433 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Long N = Long.parseLong(br.readLine());
+        bw.write(String.valueOf(factorial(N)));
+        bw.flush();
+        
+    }
+
+    static Long factorial(Long N) {
+
+        //base : 반복 종료 조건 
+        if(N == 1 || N == 0) {
+            return 1L; 
+        }
+
+        return N * factorial(N-1);
+    }
+}
