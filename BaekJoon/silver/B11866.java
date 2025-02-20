ba77c377a97d1173797bcef699b352ad1a3286eb
김희망 <gmlakd990514@naver.com>
2025-02-20 17:58:41 +0900

    feat: 백준 11866번 풀이

--- BaekJoon/silver/B11866.java
+++ BaekJoon/silver/B11866.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 
 public class B11866 {
     
@@ -23,13 +23,34 @@ public class B11866 {
 
         
         
-        Queue<Integer> q = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
         
         for(int i = 1; i<=N; i++) {
-            q.add(i);
+            list.add(i);
         }
 
 
+        // 삭제 시 인덱스가 중요함.
+        int idx = 0;
+        while(list.size()!=0) {
+
+            // index에서 부터 K-1번째 사람을 죽여야 하는데, 이때 범위를 벗어나는것을 방지하기 위해서
+            
+            idx = (idx+K-1) % N;
+            if(list.size() != 1) {
+                sb.append(list.remove(idx)).append(", ");
+            } else {
+                sb.append(list.remove(idx));
+            }
+            
+
+            // 길이가 점점 줄어드니까.. N도 줄어듦..
+            N--;
+        }
+
+        sb.append(">");
+        bw.write(sb.toString());
+        bw.flush();
     }
 
     
